2cabbd0c2935890cb77a6555d9fbb20f74abcda0
IbrahimNaserr <ibrahim.naserr@outlook.com>
2026-04-19 19:43:54 +0400

    Create AboutUs.jsx

---
+++ src/AboutUs.jsx
@@ -0,0 +1,10 @@
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Paradise Nursery</h1>
+      <p>We sell beautiful plants for your home.</p>
+    </div>
+  );
+}
+
+export default AboutUs;
